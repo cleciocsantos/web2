5d1e7028e20dd68ecd657191dc3dc78c3aabead7
Clécio Santos <91676554+cleciocsantos@users.noreply.github.com>
2024-09-19 10:25:33 -0300

    Códigos para a aula do dia 19/09/2024

--- exemplo_servidor/demo_fileserver.js
+++ exemplo_servidor/demo_fileserver.js
@@ -5,16 +5,29 @@ http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     var nomearquivo = "." + q.pathname;
     console.log("Nome do arquivo: " + nomearquivo);
-    //console.log("Nome: " + q.nome + " E-mail: " + q.email + " Senha: " + q.senha);
-  fs.readFile(nomearquivo, function(err, data) {
-    if(err){
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        return res.end("404 Arquivo não encontrado!");
+    if(nomearquivo == "./"){
+      fs.readFile("index.html", function(err, data) {
+        if(err){
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Arquivo não encontrado!");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      });
     }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+    else{
+    //console.log("Nome: " + q.nome + " E-mail: " + q.email + " Senha: " + q.senha);
+      fs.readFile(nomearquivo, function(err, data) {
+        if(err){
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Arquivo não encontrado!");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      });
+    }
 }).listen(8080, () => {
     console.log("O servidor foi iniciado na porta 8080");
 });
